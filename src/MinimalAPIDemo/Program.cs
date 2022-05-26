e16bc8dc96a270701ddacbeba5020bd414dcda43
Vitor Nikiforck Pereira <vitor.pereira@ndd.com.br>
2022-05-26 14:21:13 -0300

    added get employee by id

--- src/MinimalAPIDemo/Program.cs
+++ src/MinimalAPIDemo/Program.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using MinimalAPIDemo.Data;
 using MinimalAPIDemo.Extensions;
+using MinimalAPIDemo.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +25,16 @@ app.MapGet("/employees", async (
     MinimalContextDb context) =>
     await context.Employees.ToListAsync())
     .WithName("GetEmployees")
+    .WithTags("Employee");
+
+app.MapGet("employees/{id}", async (
+    Guid id,
+    MinimalContextDb context) =>
+    await context.Employees.FindAsync(id)
+    is Employee employee ? Results.Ok(employee) : Results.NotFound())
+    .Produces<Employee>(StatusCodes.Status200OK)
+    .Produces<Employee>(StatusCodes.Status404NotFound)
+    .WithName("GetEmployeeById")
     .WithTags("Employee");
 
 app.Run();
