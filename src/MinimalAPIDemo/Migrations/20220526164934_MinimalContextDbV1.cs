ac6bca2199505fad1e91a7a8d4d422b4deca75d8
Vitor Nikiforck Pereira <vitor.pereira@ndd.com.br>
2022-05-26 14:07:08 -0300

    added migrations

---
+++ src/MinimalAPIDemo/Migrations/20220526164934_MinimalContextDbV1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalAPIDemo.Migrations
+{
+    public partial class MinimalContextDbV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Document = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Department = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    GrossSalary = table.Column<decimal>(type: "decimal(8,2)", precision: 8, scale: 2, nullable: false),
+                    AdmissionDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HealthPlan = table.Column<bool>(type: "bit", nullable: false),
+                    DentalPlan = table.Column<bool>(type: "bit", nullable: false),
+                    TransportantionVoucher = table.Column<bool>(type: "bit", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
