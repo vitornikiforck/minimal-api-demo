af51e10a39df95f340fa305bd98b848a4f153902
Vitor Nikiforck Pereira <vitor.pereira@ndd.com.br>
2022-05-26 14:06:58 -0300

    load appsettings and contexdb methods

---
+++ src/MinimalAPIDemo/Extensions/ContextDbExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalAPIDemo.Data;
+using MinimalAPIDemo.Settings;
+
+namespace MinimalAPIDemo.Extensions
+{
+    public static class ContextDbExtensions
+    {
+        public static void AddContextDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            var appSettings = configuration.LoadSettings<AppSettings>("AppSettings");
+
+            services.AddScoped(context =>
+            {
+                var options = new DbContextOptionsBuilder<MinimalContextDb>().UseSqlServer(appSettings.ConnectionString).Options;
+                return new MinimalContextDb(options);
+            });
+
+            services.AddDbContext<MinimalContextDb>();
+        }
+    }
+}
---
+++ src/MinimalAPIDemo/Extensions/SettingsExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace MinimalAPIDemo.Extensions
+{
+    public static class SettingsExtensions
+    {
+        public static T LoadSettings<T>(this IConfiguration configuration, string sectionName, IServiceCollection service = null) where T : class
+        {
+            var sections = configuration.GetSection(sectionName);
+            service?.Configure<T>(sections);
+
+            var settings = sections.Get<T>();
+            return settings;
+        }
+    }
+}
