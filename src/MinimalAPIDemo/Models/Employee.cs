ccb60ed6a43194773d3c35132a12f5e62fe5c6e6
Vitor Nikiforck Pereira <vitor.pereira@ndd.com.br>
2022-05-26 14:07:21 -0300

    added employee model

---
+++ src/MinimalAPIDemo/Models/Employee.cs
@@ -0,0 +1,74 @@
+﻿namespace MinimalAPIDemo.Models
+{
+    public class Employee
+    {
+        /// <summary>
+        /// Identificador
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Nome
+        /// </summary>
+        public string? FirstName { get; set; }
+        /// <summary>
+        /// Sobrenome
+        /// </summary>
+        public string? LastName { get; set; }
+        /// <summary>
+        /// CPF
+        /// </summary>
+        public string? Document { get; set; }
+        /// <summary>
+        /// Setor/Departamento
+        /// </summary>
+        public string? Department { get; set; }
+        /// <summary>
+        /// Salário Bruto
+        /// </summary>
+        public decimal GrossSalary { get; set; }
+        /// <summary>
+        /// Data de Admissão
+        /// </summary>
+        public DateTime AdmissionDate { get; private set; }
+        /// <summary>
+        /// Plano de Saúde
+        /// </summary>
+        public bool HealthPlan { get; set; }
+        /// <summary>
+        /// Plano Dental
+        /// </summary>
+        public bool DentalPlan { get; set; }
+        /// <summary>
+        /// Vale Transporte
+        /// </summary>
+        public bool TransportantionVoucher { get; set; }
+        /// <summary>
+        /// Data da última modificação do funcionário
+        /// </summary>
+        public DateTime UpdateAt { get; private set; }
+        /// <summary>
+        /// Funcionário Ativo
+        /// </summary>
+        public bool Active { get; set; }
+
+        public Employee()
+        {
+            AdmissionDate = DateTime.UtcNow;
+            UpdateAt = AdmissionDate;
+        }
+
+        /// <summary>
+        /// Atualiza data de modificação e seta o funcionário como removido
+        /// </summary>
+        public void SetAsInactive()
+        {
+            SetLastModification();
+            Active = false;
+        }
+
+        /// <summary>
+        /// Seta a data de atualização do cliente para data e hora atual
+        /// </summary>
+        public void SetLastModification() => UpdateAt = DateTime.UtcNow;
+    }
+}
