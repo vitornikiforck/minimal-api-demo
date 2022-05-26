9f2d1cc94da9d93236d01a96af145a036425fa7c
Vitor Nikiforck Pereira <vitor.pereira@ndd.com.br>
2022-05-26 14:07:50 -0300

    added get employees endpoint

--- src/MinimalAPIDemo/Program.cs
+++ src/MinimalAPIDemo/Program.cs
@@ -1,13 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPIDemo.Data;
+using MinimalAPIDemo.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddContextDb(builder.Configuration);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,28 +19,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/employees", async (
+    MinimalContextDb context) =>
+    await context.Employees.ToListAsync())
+    .WithName("GetEmployees")
+    .WithTags("Employee");
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
